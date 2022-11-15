995bd15c15a5a716c7307f462bb09fe1cf8bc826
stellazhangx0 <87676450+stellazhangx0@users.noreply.github.com>
2022-11-14 21:34:20 -0800

    Update task2_part1.py

--- task2_part1.py
+++ task2_part1.py
@@ -9,11 +9,31 @@ and output the title of the song if yes
 Reduce function combines the titles of songs in each partition, and output the final song title list
 """
 
-from partition_ import readPartition, getPartitionLocations
+# from command_edit import readPartition, getPartitionLocations
+# 直接import会报错 ValueError: Need valid data path!
+import requests
+def getPartitionLocations(rurl, file_path):
+    file_path = file_path.replace('.', '/')
+    response = requests.get(
+        rurl+file_path + '.json').json()
+    if not response:
+        raise ValueError('No Such File!')
+    data_addresses = {}
+    for i in range(len(response)):
+        data_addresses['p'+str(i+1)] = response['p'+str(i+1)]
+    return data_addresses
+
+def readPartition(data_url, rurl, file_path, parition_number):
+    data_addresses = getPartitionLocations(rurl, file_path)
+    address = data_addresses.get(parition_number)
+    file_url = data_url + address
+    return requests.get(file_url).json()
 
 
 def mapPartition_spotify(file_path, partition_number, user_input):
-    partition = readPartition(file_path, partition_number)
+    rurl = 'https://project-dc1b5-default-rtdb.firebaseio.com/root'
+    durl = 'https://project-dc1b5-default-rtdb.firebaseio.com/data'
+    partition = readPartition(durl, rurl, file_path, partition_number)
     output = []
     for song in partition:
         if user_input.get('artist') in song['artist_names'] and \
@@ -28,7 +48,8 @@ def mapPartition_spotify(file_path, partition_number, user_input):
     return output
 
 def Reduce_spotify(file_path):
-    addresses = getPartitionLocations(file_path)
+    rurl = 'https://project-dc1b5-default-rtdb.firebaseio.com/root'
+    addresses = getPartitionLocations(rurl, file_path)
     songs = []
     for key in addresses.keys():
         temp = mapPartition_spotify(file_path, key, user_input)
@@ -44,17 +65,15 @@ user_input = {'artist': 'Justin Bieber', 'peak_rank': '1', 'weeks_on_chart_left'
               'weeks_on_chart_right': '1000', 'dance_left': '0.1', 'dance_right': '1', \
               'energy_left': '0', 'energy_right': '1'}
 
-file_path = '/spotify/spotify_top_charts_22_edit.csv'
-partition_number = 'p1'
-map = mapPartition_spotify(file_path, partition_number, user_input)
-print(map)
-reduce = Reduce_spotify(file_path)
-print(reduce)
+print(mapPartition_spotify('/spotify/spotify_top_charts_22_edit.csv', 'p1', user_input))
+print(Reduce_spotify('/spotify/spotify_top_charts_22_edit.csv'))
 
 
 
 def mapPartition_tiktok(file_path, partition_number, user_input):
-    partition = readPartition(file_path, partition_number)
+    rurl = 'https://project-dc1b5-default-rtdb.firebaseio.com/root'
+    durl = 'https://project-dc1b5-default-rtdb.firebaseio.com/data'
+    partition = readPartition(durl, rurl, file_path, partition_number)
     output = []
     for song in partition:
         if user_input.get('artist') in song['artist_name'] and \
@@ -66,7 +85,8 @@ def mapPartition_tiktok(file_path, partition_number, user_input):
     return output
 
 def Reduce_tiktok(file_path):
-    addresses = getPartitionLocations(file_path)
+    rurl = 'https://project-dc1b5-default-rtdb.firebaseio.com/root'
+    addresses = getPartitionLocations(rurl, file_path)
     songs = []
     for key in addresses.keys():
         temp = mapPartition_tiktok(file_path, key, user_input)
