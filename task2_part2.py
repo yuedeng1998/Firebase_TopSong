885dc5e836eeb3e10437b329b904cb8d399cfabc
stellazhangx0 <87676450+stellazhangx0@users.noreply.github.com>
2022-11-14 21:57:26 -0800

    Update task2_part2.py

--- task2_part2.py
+++ task2_part2.py
@@ -7,24 +7,23 @@ output the number of songs that appeared on Spotify or Tiktok 2022 top songs
 Reduce function then combines the local counts in each partition for number of songs, 
 and sums them up to produce final count
 '''
+from command_edit import cat_file
+from partition_ import readPartition, getPartitionLocations
 
-
-def mapPartition_TikTok(file_path, partition):
-    #file_path = 'songs-2_edit.json'
+def mapPartition_TikTok(file_path, partition_number):
+    #file_path = 'pastHotSongs/songs-2_edit.csv'
     rurl = 'https://project-dc1b5-default-rtdb.firebaseio.com/root'
     durl = 'https://project-dc1b5-default-rtdb.firebaseio.com/data'
     
-    partition = readPartition(file_path, partition)
-    tiktok = cat_file(rurl, durl, 'tiktok/TikTok_songs_2022_edit.json')
-    
+    partition = readPartition(durl, rurl, file_path, partition_number)
     title = []
     for item in partition:
         title.append(item['title'])
     
-    #cat_file返回的是dic吗
+    tiktok = cat_file(rurl, durl, 'tiktok/TikTok_songs_2022_edit.csv')
     tiktok_songs = []
-    for key, value in tiktok.items():
-        tiktok_songs.append(value['track_name'])
+    for song in tiktok:
+        tiktok_songs.append(song['track_name'])
     
     rep = list(set(title).intersection(tiktok_songs))
     return len(rep)
@@ -37,21 +36,18 @@ def Reduce_Tiktok(rurl, file_path):
     return count
 
 def mapPartition_Spotify(file_path, partition):
-    #file_path = 'songs-2_edit.json'
     rurl = 'https://project-dc1b5-default-rtdb.firebaseio.com/root'
     durl = 'https://project-dc1b5-default-rtdb.firebaseio.com/data'
     
-    partition = readPartition(file_path, partition)
-    spotify = cat_file(rurl, durl, 'spotify/spotify_top_charts_22_edit.json')
-    
+    partition = readPartition(durl, rurl, file_path, partition)
     title = []
     for item in partition:
         title.append(item['title'])
     
-    #cat_file返回的是dic吗
+    spotify = cat_file(rurl, durl, 'spotify/spotify_top_charts_22_edit.csv')
     spotify_songs = []
-    for key, value in spotify.items():
-        spotify_songs.append(value['track_name'])
+    for song in spotify:
+        spotify_songs.append(song['track_name'])
     
     rep = list(set(title).intersection(spotify_songs))
     return len(rep)
@@ -62,3 +58,8 @@ def Reduce_Spotify(rurl, file_path):
     for key in addresses.keys():
         count += mapPartition_Spotify(file_path, key)
     return count
+
+rurl = 'https://project-dc1b5-default-rtdb.firebaseio.com/root'
+file_path = 'pastHotSongs/songs-2_edit.csv'
+partition_number = 'p1'
+mapPartition_Spotify(file_path, partition_number)
