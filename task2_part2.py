e0d4b1ceb417e2377356ea41e2843b5e67cb3668
stellazhangx0 <87676450+stellazhangx0@users.noreply.github.com>
2022-11-15 15:01:29 -0800

    Update task2_part2.py

--- task2_part2.py
+++ task2_part2.py
@@ -7,8 +7,36 @@ output the number of songs that appeared on Spotify or Tiktok 2022 top songs
 Reduce function then combines the local counts in each partition for number of songs, 
 and sums them up to produce final count
 '''
-from command_edit import cat_file
-from partition_ import readPartition, getPartitionLocations
+# from command_edit import cat_file, readPartition, getPartitionLocations
+# 直接import会报错 ValueError: Need valid data path!
+import requests
+def getPartitionLocations(rurl, file_path):
+    file_path = file_path.replace('.', '/')
+    response = requests.get(
+        rurl+file_path + '.json').json()
+    if not response:
+        raise ValueError('No Such File!')
+    data_addresses = {}
+    for i in range(len(response)):
+        data_addresses['p'+str(i+1)] = response['p'+str(i+1)]
+    return data_addresses
+
+def readPartition(data_url, rurl, file_path, partition_number):
+    data_addresses = getPartitionLocations(rurl, file_path)
+    address = data_addresses.get(partition_number)
+    file_url = data_url + address
+    return requests.get(file_url).json()
+
+def cat_file(rurl, durl, file_path):
+    data_addresses = getPartitionLocations(rurl, file_path)
+    file_content = []
+    for value in data_addresses.values():
+        file_url = durl + value
+        file_content.extend(requests.get(file_url).json())
+    return file_content
+
+
+
 
 def mapPartition_TikTok(file_path, partition_number):
     #file_path = 'pastHotSongs/songs-2_edit.csv'
@@ -20,7 +48,7 @@ def mapPartition_TikTok(file_path, partition_number):
     for item in partition:
         title.append(item['title'])
     
-    tiktok = cat_file(rurl, durl, 'tiktok/TikTok_songs_2022_edit.csv')
+    tiktok = cat_file(rurl, durl, '/tiktok/TikTok_songs_2022_edit.csv')
     tiktok_songs = []
     for song in tiktok:
         tiktok_songs.append(song['track_name'])
@@ -44,7 +72,7 @@ def mapPartition_Spotify(file_path, partition):
     for item in partition:
         title.append(item['title'])
     
-    spotify = cat_file(rurl, durl, 'spotify/spotify_top_charts_22_edit.csv')
+    spotify = cat_file(rurl, durl, '/spotify/spotify_top_charts_22_edit.csv')
     spotify_songs = []
     for song in spotify:
         spotify_songs.append(song['track_name'])
@@ -59,7 +87,14 @@ def Reduce_Spotify(rurl, file_path):
         count += mapPartition_Spotify(file_path, key)
     return count
 
+
+
+durl = 'https://project-dc1b5-default-rtdb.firebaseio.com/data'
 rurl = 'https://project-dc1b5-default-rtdb.firebaseio.com/root'
-file_path = 'pastHotSongs/songs-2_edit.csv'
-partition_number = 'p1'
-mapPartition_Spotify(file_path, partition_number)
+file_path = '/hotSong/pastHotSongs.csv'
+partition_number = 'p2'
+print(mapPartition_TikTok(file_path, partition_number))
+print(Reduce_Tiktok(rurl, file_path))
+partition_number = 'p10'
+print(mapPartition_Spotify(file_path, partition_number))
+print(Reduce_Spotify(rurl, file_path))
